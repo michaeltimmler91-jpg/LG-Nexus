2c99ec98a276cf0a26f9f64b536f57a24d1919df
michaeltimmler91-jpg <michaeltimmler91@googlemail.com>
2026-08-10 18:54:00 +0200

    Mount Medical medications

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import AccountAdminMount from './AccountAdminMount'
 import PermissionNavigationGuard from './PermissionNavigationGuard'
 import ServiceNavigationMount from './ServiceNavigationMount'
 import MedicalModuleMount from './MedicalModuleMount'
+import MedicalMedicationMount from './MedicalMedicationMount'
 import './styles.css'
 import './auth.css'
 import './account-layout-fix.css'
@@ -14,6 +15,7 @@ import './service-navigation.css'
 import './city-organization-admin.css'
 import './medical-module.css'
 import './medical-clinical.css'
+import './medical-medications.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -22,5 +24,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <PermissionNavigationGuard />
     <ServiceNavigationMount />
     <MedicalModuleMount />
+    <MedicalMedicationMount />
   </React.StrictMode>,
 )
