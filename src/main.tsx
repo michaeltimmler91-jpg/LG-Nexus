1a7aaff0aeffed6b8653dd69244689454541355e
michaeltimmler91-jpg <michaeltimmler91@googlemail.com>
2026-08-10 16:46:55 +0200

    Mount city hall account approvals

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import NexusRoot from './NexusRoot'
+import AccountAdminMount from './AccountAdminMount'
 import './styles.css'
 import './auth.css'
 import './account-layout-fix.css'
+import './account-admin.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <NexusRoot />
+    <AccountAdminMount />
   </React.StrictMode>,
 )
