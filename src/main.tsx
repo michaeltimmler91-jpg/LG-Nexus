3d821a0330c01336f043b99faa9a0ce33bf2e99f
michaeltimmler91-jpg <michaeltimmler91@googlemail.com>
2026-08-10 07:49:55 +0200

    Use real Nexus auth root

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import NexusRoot from './NexusRoot'
 import './styles.css'
+import './auth.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <NexusRoot />
   </React.StrictMode>,
 )
