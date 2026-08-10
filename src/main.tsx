2d28d18400a85eded57e9a5c9e4cfc8284d3fcf8
michaeltimmler91-jpg <michaeltimmler91@googlemail.com>
2026-08-10 16:35:18 +0200

    Load account page layout fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import NexusRoot from './NexusRoot'
 import './styles.css'
 import './auth.css'
+import './account-layout-fix.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
