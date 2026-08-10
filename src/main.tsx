5f2d61a89a371012bb306729411acc51f49a3b9e
michaeltimmler91-jpg <michaeltimmler91@googlemail.com>
2026-08-10 16:58:28 +0200

    Enable live permission navigation

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import NexusRoot from './NexusRoot'
 import AccountAdminMount from './AccountAdminMount'
+import PermissionNavigationGuard from './PermissionNavigationGuard'
 import './styles.css'
 import './auth.css'
 import './account-layout-fix.css'
 import './account-admin.css'
+import './permission-navigation.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <NexusRoot />
     <AccountAdminMount />
+    <PermissionNavigationGuard />
   </React.StrictMode>,
 )
