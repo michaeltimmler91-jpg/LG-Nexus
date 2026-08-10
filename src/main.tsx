7a5f05f2a51734012abd5e3a10a66d138713f844
michaeltimmler91-jpg <michaeltimmler91@googlemail.com>
2026-08-10 17:18:11 +0200

    Mount City Hall and Justice navigation

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,19 @@ import ReactDOM from 'react-dom/client'
 import NexusRoot from './NexusRoot'
 import AccountAdminMount from './AccountAdminMount'
 import PermissionNavigationGuard from './PermissionNavigationGuard'
+import ServiceNavigationMount from './ServiceNavigationMount'
 import './styles.css'
 import './auth.css'
 import './account-layout-fix.css'
 import './account-admin.css'
 import './permission-navigation.css'
+import './service-navigation.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <NexusRoot />
     <AccountAdminMount />
     <PermissionNavigationGuard />
+    <ServiceNavigationMount />
   </React.StrictMode>,
 )
